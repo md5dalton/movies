d80baec213984687b86a473c1f3db82f256dee31
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-11 21:18:33 +0200

    Create signIn.jsx

---
+++ app/(auth)/signIn.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+
+export default () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-3xl font-pblack">sign in</Text>
+        </View>
+    )
+}
